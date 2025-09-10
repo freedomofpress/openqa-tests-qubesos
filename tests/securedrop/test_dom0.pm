6d3f5654b3cc9cd47e67aac39c099233f7abe38d
deeplow <francisco@freedom.press>
2025-09-10 10:52:10 +0100

    run "make test-deps" for securedrop workstation
    After all xvfb is necessary because we're now running the tests in the
    root console as a fix for [1]. Instead of adding yet another dependency
    directly in OpenQA, this now just installs whichever dependencies are
    specified in the workstation repo via the "test-deps" make target.
    
    [1]: https://github.com/freedomofpress/securedrop-workstation/pull/1414

--- tests/securedrop/test_dom0.pm
+++ tests/securedrop/test_dom0.pm
@@ -30,11 +30,11 @@ sub run {
 
     my $sdw_path = "/home/user/securedrop-workstation";
 
-    # Setup testing requirements and run tests
-    assert_script_run('rpm -q python3-pytest || sudo qubes-dom0-update -y python3-pytest', timeout => 300);
-    assert_script_run('rpm -q python3-pytest-cov || sudo qubes-dom0-update -y python3-pytest-cov', timeout => 300);
     script_run('ln -s /usr/share/securedrop-workstation-dom0-config/config.json /home/user/securedrop-workstation/config.json');
     script_run('ln -s /usr/share/securedrop-workstation-dom0-config/sd-journalist.sec /home/user/securedrop-workstation/sd-journalist.sec');
+
+    # Setup testing requirements and run tests
+    assert_script_run("alias dnf=qubes-dom0-update && make -C $sdw_path test-deps", timeout => 300);
     script_run("env CI=true make -C $sdw_path test | tee make-test.log", timeout => 2400);
 
 
