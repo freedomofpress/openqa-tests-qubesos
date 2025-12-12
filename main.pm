f0d2b0519c9da47416cc7a1067627427fe727b99
deeplow <francisco@freedom.press>
2025-12-12 15:18:23 +0000

    Move preload activation to section after upgrade
    Tests were failing [1] with:
    
        # Test died: command 'sudo qubesctl top.enable qvm.disposable-preload pillar=True' failed at /usr/lib/os-autoinst/autotest.pm line 515.
    
    This was due to the fact that the 4.3 test modifications had originally
    assumed a 4.3 system. Since the aim is to now support an inplace upgrade
    to 4.3, this section must run after the upgrade.
    
    [1]: https://openqa.qubes-os.org/tests/162233#step/install_workstation/15

--- main.pm
+++ main.pm
@@ -191,6 +191,10 @@ if (get_var('SYSTEM_TESTS')) {
     autotest::loadtest "tests/system_tests.pm";
 }
 
+
+if (check_var('SECUREDROP_INSTALL', '1')) {
+    autotest::loadtest("tests/securedrop/install_workstation_post.pm");
+}
 if (check_var('SECUREDROP_TEST', "test_dom0")) {
     autotest::loadtest("tests/securedrop/test_dom0.pm");
 } elsif (check_var('SECUREDROP_TEST', "test_gui")) {
